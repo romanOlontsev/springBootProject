4739f2993303b1b9a59759076110f3c8aa698b62
romanOlontsev <roverviner@yandex.ru>
2022-07-13 22:51:57 +0300

    minor registration form changes

--- src/main/java/com/viner/site/web/dto/AddUserDto.java
+++ src/main/java/com/viner/site/web/dto/AddUserDto.java
@@ -3,23 +3,19 @@ package com.viner.site.web.dto;
 import com.viner.site.entity.Role;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
 public class AddUserDto {
 
-    @Size(min = 5, max = 25, message = "Your username must be between 5 and 25 characters long")
+    @Size(min = 3, max = 25, message = "Your username must be between 3 and 25 characters long")
     private String username;
 
-    @NotNull
+    @Size(min = 5, max = 25, message = "Your password must be between 5 and 25 characters long")
     private String password;
 
     private Set<Role> roles;
-
-    public String getUsername() {
-        return username;
-    }
 }
